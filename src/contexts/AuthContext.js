d041d60acf0b6e49e50602d8f4b1381127e00a8f
Marouf <maroufibrahimi7264@gmail.com>
2023-08-31 14:45:44 -0700

    create auth provider and authenticate email & pass

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  useEffect(() => {
+    const unsubscriber = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    })
+
+    return unsubscriber
+  }, [])
+
+  const value = {
+    currentUser,
+    signup,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
